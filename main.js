9a3cf39ef81ade6e2bffae5eada85cda61978c22
Christian <christianmateus016@gmail.com>
2022-08-22 00:19:41 -0300

    First version added 0.0.1

---
+++ main.js
@@ -0,0 +1,86 @@
+// Modules
+const { app, BrowserWindow, dialog, ipcMain } = require('electron');
+const fs = require('fs');
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+
+// Create a new BrowserWindow when `app` is ready
+function createWindow() {
+
+  mainWindow = new BrowserWindow({
+    width: 1000, height: 500,
+    autoHideMenuBar: true,
+    webPreferences: {
+      contextIsolation: false,
+      nodeIntegration: true,
+      preload: 'preload.js'
+    }
+
+  })
+
+  // Load index.html into the new BrowserWindow
+  mainWindow.loadFile('index.html');
+
+  // Open DevTools - Remove for PRODUCTION!
+  // mainWindow.webContents.openDevTools();
+
+  // Listen for window being closed
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+}
+
+// Electron `app` is ready
+app.on('ready', createWindow)
+
+// Quit when all windows are closed - (Not macOS - Darwin)
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+// When app icon is clicked and app is running, (macOS) recreate the BrowserWindow
+app.on('activate', () => {
+  if (mainWindow === null) createWindow()
+})
+
+/* ================== 
+    FUNCTIONS
+   ================== */
+
+// Function for sending file path
+
+ipcMain.on("openfile", () => {
+  dialog.showOpenDialog(mainWindow, {
+    filters: [{
+      name: "ITA files", extensions: ["ITA"]
+      // name: "All files", extensions: ["*"]
+    }], properties: ["openFile"]
+  })
+    .then((um) => {
+      let joao = um.filePaths.toString();
+      // joao = joao.replace(/\\/g, "/") //Used to 
+      mainWindow.webContents.send("dialog", joao);
+      console.log(joao)
+    })
+})
+
+// Saving actual file
+ipcMain.on("saveFile", (e, arg) => {
+  dialog.showSaveDialog(mainWindow,
+    {
+      filters: [
+        { name: 'ITA Files', extensions: ['ITA'] }
+      ]
+    }).then((dados) => {
+      let salvar = dados.filePath.toString();
+      mainWindow.webContents.send("savefile", salvar);
+      console.log(salvar)
+    })
+})
+
+// Quiting application
+ipcMain.on("quitApp", (e, arg) => {
+  mainWindow.close();
+})
---
+++ renderer.js
@@ -0,0 +1,493 @@
+const fs = require('fs');
+const { windowsStore } = require('process');
+const { ipcRenderer } = require('electron');
+
+// Const for testing text output
+const textarea = document.getElementById("testes");
+const closeBtn = document.getElementById("closeFile")
+const saveBtn = document.getElementById("saveFile")
+
+openFile.addEventListener("click", () => {
+    ipcRenderer.send("openfile")
+    let table = document.querySelector("table");
+    while (table.rows.length > 2) {
+        table.deleteRow(1);
+    }
+})
+
+saveBtn.addEventListener("click", () => {
+    ipcRenderer.send("saveFile")
+})
+
+quitApp.addEventListener("click", () => {
+    ipcRenderer.send("quitApp")
+})
+
+ipcRenderer.on("dialog", (e, arg) => {
+    let fd = fs.openSync(arg);
+
+    var contador = 176;
+    var somador = 0;
+    var seq = 1;
+
+    // ===============
+    // ---- HEADER
+    // ===============
+
+    let program = function () {
+        // Buffer for reading HEADER
+        let buffer_header = Buffer.alloc(16);// 16 bytes
+
+        fs.read(fd, buffer_header, 0, 16, 0, function (err, bytesread, buffer_header) {
+            // Getting values
+            var total_item = buffer_header.readInt8(6);
+            // Setting values
+            var cont = document.getElementById("count");
+            cont.setAttribute("value", total_item);
+
+            for (i = 1; i <= total_item; i++) {
+
+                if (i == 1) {
+                    contador = 16;
+                    somador = 0;
+                    chunk();
+                } else if (i == 2) {
+                    setTimeout(() => { somador = somador + 176; cloneRow(); }, 150);
+                } else if (i == 3) {
+                    setTimeout(() => {
+                        somador = somador + 176
+                        cloneRow();
+                        ipcRenderer.on("savefile", (e, arg) => {
+                            fs.writeFileSync(arg, buffer)
+                            fs.writeFileSync(arg, bufferGeral)
+                        })
+                    }, 300);
+                } else if (i == 4) {
+                    setTimeout(() => {
+                        somador = somador + 176
+                        cloneRow();
+                        ipcRenderer.on("savefile", (e, arg) => {
+                            fs.writeFileSync(arg, bufferGeral)
+                        })
+                    }, 450);
+                } else if (i == 5) {
+                    setTimeout(() => {
+                        somador = somador + 176
+                        cloneRow();
+                    }, 600);
+                } else if (i == 6) {
+                    setTimeout(() => {
+                        somador = somador + 176
+                        cloneRow();
+                    }, 750);
+                } else if (i == 7) {
+                    setTimeout(() => {
+                        somador = somador + 176
+                        cloneRow();
+                    }, 900);
+                } else if (i == 8) {
+                    setTimeout(() => {
+                        somador = somador + 176
+                        cloneRow();
+                    }, 1050);
+                } else if (i == 9) {
+                    setTimeout(() => {
+                        somador = somador + 176
+                        cloneRow();
+                    }, 1200);
+                } else if (i == 10) {
+                    setTimeout(() => {
+                        somador = somador + 176
+                        cloneRow();
+                    }, 1350);
+                } else if (i == 11) {
+                    setTimeout(() => {
+                        somador = somador + 176
+                        cloneRow();
+                    }, 1500);
+                } else if (i == 12) {
+                    setTimeout(() => {
+                        somador = somador + 176
+                        cloneRow();
+                    }, 1650);
+                } else if (i == 13) {
+                    setTimeout(() => {
+                        somador = somador + 176
+                        cloneRow();
+                    }, 1800);
+                } else if (i == 14) {
+                    setTimeout(() => {
+                        somador = somador + 176
+                        cloneRow();
+                    }, 1950);
+                } else if (i == 15) {
+                    setTimeout(() => {
+                        somador = somador + 176
+                        cloneRow();
+                    }, 2100);
+                } else if (i == 16) {
+                    setTimeout(() => {
+                        somador = somador + 176
+                        cloneRow();
+                    }, 2250);
+                } else if (i == 17) {
+                    setTimeout(() => {
+                        somador = somador + 176
+                        cloneRow();
+                    }, 2400);
+                } else if (i == 18) {
+                    setTimeout(() => {
+                        somador = somador + 176
+                        cloneRow();
+                    }, 2550);
+                } else if (i == 19) {
+                    setTimeout(() => {
+                        somador = somador + 176
+                        cloneRow();
+                    }, 2700);
+                } else if (i == 20) {
+                    setTimeout(() => {
+                        somador = somador + 176
+                        cloneRow();
+                    }, 2850);
+                } else if (i == 21) {
+                    setTimeout(() => {
+                        somador = somador + 176
+                        cloneRow();
+                    }, 3000);
+                } else if (i == 22) {
+                    setTimeout(() => {
+                        somador = somador + 176
+                        cloneRow();
+                    }, 3150);
+                } else if (i == 23) {
+                    setTimeout(() => {
+                        somador = somador + 176
+                        cloneRow();
+                    }, 3300);
+                } else if (i == 24) {
+                    setTimeout(() => {
+                        somador = somador + 176
+                        cloneRow();
+                    }, 3450);
+                } else if (i == 25) {
+                    setTimeout(() => {
+                        somador = somador + 176
+                        cloneRow();
+                    }, 3600);
+                } else if (i == 26) {
+                    setTimeout(() => {
+                        somador = somador + 176
+                        cloneRow();
+                    }, 3750);
+                } else if (i == 27) {
+                    setTimeout(() => {
+                        somador = somador + 176
+                        cloneRow();
+                    }, 3900);
+                } else if (i == 28) {
+                    setTimeout(() => {
+                        somador = somador + 176
+                        cloneRow();
+                    }, 4050);
+                } else if (i == 29) {
+                    setTimeout(() => {
+                        somador = somador + 176
+                        cloneRow();
+                    }, 4200);
+                } else if (i == 30) {
+                    setTimeout(() => {
+                        somador = somador + 176
+                        cloneRow();
+                    }, 4350);
+                } else if (i == 31) {
+                    setTimeout(() => {
+                        somador = somador + 176
+                        cloneRow();
+                    }, 4500);
+                } else if (i == 32) {
+                    setTimeout(() => {
+                        somador = somador + 176
+                        cloneRow();
+                    }, 4650);
+                } else if (i == 33) {
+                    setTimeout(() => {
+                        somador = somador + 176
+                        cloneRow();
+                    }, 4800);
+                } else if (i == 34) {
+                    setTimeout(() => {
+                        somador = somador + 176
+                        cloneRow();
+                    }, 4950);
+                } else if (i == 35) {
+                    setTimeout(() => {
+                        somador = somador + 176
+                        cloneRow();
+                    }, 5100);
+                } else if (i == 36) {
+                    setTimeout(() => {
+                        somador = somador + 176
+                        cloneRow();
+                    }, 5250);
+                }
+            }
+        })
+    }
+
+    // ===============
+    // ---- CHUNKS
+    // ===============
+
+    // Buffer for reading CHUNK DATA
+    let buffer = Buffer.alloc(176); // 176 bytes
+
+    let chunk = function () {
+        fs.read(fd, buffer, 0, 176, contador, function (err, bytesread, buffer) {
+
+            var seq = document.querySelector(".number-sequential");
+            seq.innerText = 01;
+
+            // Editable fields
+            getItem_index = buffer.readInt8(54);
+            getInside = buffer.readInt8(70);
+            getETS = buffer.readUInt8(71);
+            getItem_id = buffer.readUInt8(132);
+            getQuantity = buffer.readInt16LE(136);
+            getRandom = buffer.readUInt8(133);
+            getGlow = buffer.readUInt8(140);
+            getStatus = buffer.readUInt8(144);
+            getItemX = buffer.readFloatLE(96).toFixed(2);
+            getItemY = buffer.readFloatLE(100).toFixed(2);
+            getItemZ = buffer.readFloatLE(104).toFixed(2);
+
+            // Set value for Index
+            var item_index = document.querySelector(".item-index");
+            item_index.innerHTML = getItem_index;
+
+            // Set value for Inside
+            var inside = document.querySelector(".inside");
+            if (getInside == 0) {
+                inside.selectedIndex = 0;
+            } else if (getInside == 1) {
+                inside.selectedIndex = 1;
+            } else {
+                inside.selectedIndex = 2;
+            }
+
+            // Set value for ETS
+            var ETS = document.querySelector(".ets");
+            ETS.innerText = getETS;
+
+            // Set value for Quantity
+            var quantity = document.querySelector(".quantity");
+            quantity.innerHTML = getQuantity;
+
+            // Set value for Random
+            var random = document.querySelector(".random");
+            if (getRandom == 16) {
+                random.selectedIndex = 0;
+            } else {
+                random.selectedIndex = 1;
+            }
+
+            // Set value for Glow
+            var glow = document.querySelector(".glow");
+            if (getGlow == 0) {
+                glow.selectedIndex = 0;
+            } else if (getGlow == 1) {
+                glow.selectedIndex = 1;
+            } else if (getGlow == 2) {
+                glow.selectedIndex = 2;
+            } else if (getGlow == 3) {
+                glow.selectedIndex = 3;
+            } else if (getGlow == 4) {
+                glow.selectedIndex = 4;
+            } else if (getGlow == 5) {
+                glow.selectedIndex = 5;
+            } else if (getGlow == 6) {
+                glow.selectedIndex = 6;
+            } else if (getGlow == 7) {
+                glow.selectedIndex = 7;
+            } else if (getGlow == 8) {
+                glow.selectedIndex = 8;
+            } else if (getGlow == 9) {
+                glow.selectedIndex = 9;
+            }
+
+            // Set value for Item Status
+            var status = document.querySelector(".status");
+            if (getStatus == 16) {
+                status.selectedIndex = 2;
+            } else if (getStatus == 2 || getStatus == 6) {
+                status.selectedIndex = 1;
+            } else {
+                status.selectedIndex = 0;
+            }
+
+            // Set value for ID (Item name)
+            var item_id = document.querySelector(".item-id");
+            for (var i = 0; i < item_id.options.length; i++) {
+                if (item_id.options[i].value == getItem_id) {
+                    item_id.selectedIndex = i;
+                    // break;
+                }
+            }
+
+            // Set value for X Coordinate
+            var itemX = document.querySelector(".itemX");
+            itemX.innerText = getItemX;
+            // Set value for Y Coordinate
+            var itemY = document.querySelector(".itemY");
+            itemY.innerText = getItemY;
+            // Set value for Z Coordinate
+            var itemZ = document.querySelector(".itemZ");
+            itemZ.innerText = getItemZ;
+        })
+    }
+
+    program();
+
+    var bufferGeral = Buffer.alloc(176);
+    var row = document.querySelector(".item-row"); // Finding row to copy
+    var table = document.querySelector("table"); // Finding table to append to
+
+    // Função para criar novas linhas
+    function cloneRow() {
+
+        var clone = row.cloneNode(true); // Cloning all child nodes
+        clone.querySelector(".number-sequential").innerText = seq = seq + 1;
+
+        // Reading next INDEX byte
+        fs.read(fd, bufferGeral, 0, bufferGeral.length, 16 + somador, (err, bytesread, buffer) => {
+            let cloneIndex = buffer.readInt8(54);
+            // buffer.writeInt8; USAR ISSO PARA SALVAR ALTERAÇÕES
+            clone.querySelector(".item-index").innerHTML = cloneIndex;
+        })
+        //Reading next INSIDE bytes
+        fs.read(fd, bufferGeral, 0, bufferGeral.length, 16 + somador, (err, bytesread, buffer) => {
+            let cloneInside = buffer.readUInt8(70);
+
+            if (cloneInside == 0) {
+                clone.querySelector(".inside").selectedIndex = 0;
+            } else if (cloneInside == 1) {
+                clone.querySelector(".inside").selectedIndex = 1;
+            } else {
+                clone.querySelector(".inside").selectedIndex = 2;
+            }
+        })
+
+        // Reading next ETS byte
+        fs.read(fd, bufferGeral, 0, bufferGeral.length, 16 + somador, (err, bytesread, buffer) => {
+            let cloneETS = buffer.readUInt8(71);
+            clone.querySelector(".ets").innerHTML = cloneETS;
+        })
+
+        //Reading next QUANTITY bytes
+        fs.read(fd, bufferGeral, 0, bufferGeral.length, 16 + somador, (err, bytesread, buffer) => {
+            let cloneQuantity = buffer.readInt16LE(136);
+            clone.querySelector(".quantity").innerHTML = cloneQuantity;
+        })
+
+        //Reading next RANDOM bytes
+        fs.read(fd, bufferGeral, 0, bufferGeral.length, 16 + somador, (err, bytesread, buffer) => {
+            let cloneRandom = buffer.readUInt8(133);
+
+            if (cloneRandom == 16) {
+                clone.querySelector(".random").selectedIndex = 0;
+            } else {
+                clone.querySelector(".random").selectedIndex = 1;
+            }
+        })
+
+        //Reading next GLOW bytes
+        fs.read(fd, bufferGeral, 0, bufferGeral.length, 16 + somador, (err, bytesread, buffer) => {
+            let cloneGlow = buffer.readUInt8(140);
+            clone.querySelector(".glow");
+
+            if (cloneGlow == 0) {
+                clone.querySelector(".glow").selectedIndex = 0;
+            } else if (cloneGlow == 1) {
+                clone.querySelector(".glow").selectedIndex = 1;
+            } else if (cloneGlow == 2) {
+                clone.querySelector(".glow").selectedIndex = 2;
+            } else if (cloneGlow == 3) {
+                clone.querySelector(".glow").selectedIndex = 3;
+            } else if (cloneGlow == 4) {
+                clone.querySelector(".glow").selectedIndex = 4;
+            } else if (cloneGlow == 5) {
+                clone.querySelector(".glow").selectedIndex = 5;
+            } else if (cloneGlow == 6) {
+                clone.querySelector(".glow").selectedIndex = 6;
+            } else if (cloneGlow == 7) {
+                clone.querySelector(".glow").selectedIndex = 7;
+            } else if (cloneGlow == 8) {
+                clone.querySelector(".glow").selectedIndex = 8;
+            } else if (cloneGlow == 9) {
+                clone.querySelector(".glow").selectedIndex = 9;
+            }
+        })
+
+        //Reading next ITEM STATUS bytes
+        fs.read(fd, bufferGeral, 0, bufferGeral.length, 16 + somador, (err, bytesread, buffer) => {
+            let cloneStatus = buffer.readUInt8(144);
+
+            if (cloneStatus == 16) {
+                clone.querySelector(".status").selectedIndex = 2;
+            } else if (cloneStatus == 2 || cloneStatus == 6) {
+                clone.querySelector(".status").selectedIndex = 1;
+            } else {
+                clone.querySelector(".status").selectedIndex = 0;
+            }
+        })
+
+        //Reading next ITEM ID bytes
+        fs.read(fd, bufferGeral, 0, bufferGeral.length, 16 + somador, (err, bytesread, buffer) => {
+            let cloneItem_id = buffer.readUInt8(132);
+            let selectItem_id = clone.querySelector(".item-id");
+
+            for (var i = 0; i < selectItem_id.options.length; i++) {
+                if (selectItem_id.options[i].value == cloneItem_id) {
+                    selectItem_id.selectedIndex = i;
+                }
+            }
+        })
+
+        //Reading next X Coordinate bytes
+        fs.read(fd, bufferGeral, 0, bufferGeral.length, 16 + somador, (err, bytesread, buffer) => {
+            let cloneX = buffer.readFloatLE(96).toFixed(2);
+            clone.querySelector(".itemX").innerHTML = cloneX;
+        })
+
+        //Reading next Y Coordinate bytes
+        fs.read(fd, bufferGeral, 0, bufferGeral.length, 16 + somador, (err, bytesread, buffer) => {
+            let cloneY = buffer.readFloatLE(100).toFixed(2);
+            clone.querySelector(".itemY").innerHTML = cloneY;
+        })
+
+        //Reading next Z Coordinate bytes
+        fs.read(fd, bufferGeral, 0, bufferGeral.length, 16 + somador, (err, bytesread, buffer) => {
+            let cloneZ = buffer.readFloatLE(104).toFixed(2);
+            clone.querySelector(".itemZ").innerHTML = cloneZ;
+        })
+
+        table.appendChild(clone); // add new row to end of table
+
+    }
+
+    //Receiving save data path
+
+
+    //Menu buttons
+    closeBtn.addEventListener("click", closeFile);
+
+    //Functions
+    function closeFile() {
+        fs.closeSync(fd);
+        let table = document.querySelector("table");
+        while (table.rows.length > 2) {
+            table.deleteRow(1);
+        }
+
+    }
+})
